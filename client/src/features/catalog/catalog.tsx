25c76548a042ec34841b6fec609aef7033765aaf
Christopher Waters <chris.isaac.waters@gmail.com>
2022-09-26 20:13:24 -0400

    End of Section 4

--- client/src/features/catalog/catalog.tsx
+++ client/src/features/catalog/catalog.tsx
@@ -2,24 +2,18 @@ import { useState, useEffect } from 'react';
 import { Product } from '../../app/models/products';
 import ProductList from './ProductList';
 
-
-
 export default function Catalog() {
-    
-    const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
-    useEffect(() => {
-      fetch('https://localhost:5001/api/products')
-        .then((response) => response.json())
-    
-    
-        
-        .then((data) => setProducts(data));
-    }, []);
-    
-    return (
-        <>
-        <ProductList products={products}/>
-        </>
-    )
+  useEffect(() => {
+    fetch('https://localhost:5001/api/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  );
 }
